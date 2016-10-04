73a4bb2a1e739dc748f6cc45ba6d85b030fffe57
Nathan Crum <nathan.crum@outlook.com>
2016-10-04 13:53:44 -0400

    Had to call a function and pass the retrieved data from within the ajax call to work with the information.

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@
  * Global Variables
  */
 var homes;
+var homeArray = [];
 var zones;
+var zoneArray = [];
 
+/*
+ * AJAX Requests
+ */
 var xhr_zones = new XMLHttpRequest();
 xhr_zones.onreadystatechange = function () {
     if(xhr_zones.readyState === 4) {
         if (xhr_zones.status === 200){
-            console.log("01 - Ajax request for zoning_data.json");
             var data = JSON.parse(xhr_zones.responseText);
-            zones = data;
-            console.log("Zipcode pulled from first record: " + zones.features[0].properties.ZIPCODE);
+            createZoningList(data);
         } else {
             alert(xhr_zones.response);
         }
@@ -25,10 +28,8 @@ var xhr_homes = new XMLHttpRequest();
 xhr_homes.onreadystatechange = function () {
     if(xhr_homes.readyState === 4) {
         if(xhr_homes.status === 200) {
-            console.log("02 - Ajax request for homes.json");
             var data = JSON.parse(xhr_homes.responseText);
-            homes = data;
-            console.log("Zipcode pulled from first record: " + homes[0].zip);
+            createHomesList(data);
         } else {
             alert(xhr_homes.response);
         }
@@ -37,8 +38,55 @@ xhr_homes.onreadystatechange = function () {
 xhr_homes.open('GET', 'data/homes.json');
 xhr_homes.send();
 
-console.log("03 - Attempt to log zoning data outside of Ajax");
-//console.log("Zipcode pulled from first record: " + zones.features[0].properties.ZIPCODE);
+/*
+ * Custom functions
+ */
+function createZoningList(data) {
+    zones = data;
+    var zoneAddress;
+    for (var i = 0; i< zones.features.length; i++) {
+         zoneAddress = zones.features[i].properties.HOUSENO + " ";
 
-console.log("04 - Attempt to log homes data outside of Ajax");
-console.log("Zipcode pulled from first record: " + homes[0].county);
+         if (zones.features[i].properties.DIR != " "){
+             zoneAddress += zones.features[i].properties.DIR + " ";
+         }
+
+         zoneAddress += zones.features[i].properties.STRNAME + " ";
+         
+         if(zones.features[i].properties.TYPE != " "){
+             zoneAddress += zones.features[i].properties.TYPE;
+         }
+         
+         zoneAddress += ", " + zones.features[i].properties.MUNI_NAME + ", KY " + zones.features[i].properties.ZIPCODE;
+
+        zoneArray.push({
+            "address": zoneAddress,
+            "zone": zones.features[i].properties.ZONING_COD
+        });
+    }
+    
+}
+
+function createHomesList(data){
+    homes = data;
+    var homeAddress;
+    for (var i = 0; i < homes.length; i++) {
+        homeAddress = homes[i].street_number + " ";
+        
+        if(homes[i].street_dir != "") {
+            homeAddress += homes[i].street_dir + " ";
+        }
+
+        homeAddress += homes[i].street + " " + homes[i].street_suffix + ", " + homes[i].city + ", " + homes[i].state + " " + homes[i].zip;
+        homeArray.push(homeAddress);
+    }
+}
+
+function main(){
+    for(var a = 0; a < homeArray.length; a++) {
+        for(var z = 0; z < zoneArray.length; z++){
+            console.log(homeArray[a].toLowerCase());
+        }
+    }
+}
+main();
