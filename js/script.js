3f8d7888dff5200cccda30988c232c56bcf3ef54
Nathan Crum <nathan.crum@outlook.com>
2016-10-04 08:53:45 -0400

    Simplified to just the Ajax requests and some log attempts for debugging

--- js/script.js
+++ js/script.js
@@ -5,49 +5,40 @@
 var homes;
 var zones;
 
-/*
- *
- * Main function to execute the app and call all other functions.
- * 
- */
-
-function main() {
-    getHomes();
-    console.log(homes[0].state);
-}
-
-
-function getZones() {
-    var xhr_zones = new XMLHttpRequest();
-    xhr_zones.onreadystatechange = function () {
-        if(xhr_zones.readyState === 4) {
-            if (xhr_zones.status === 200){
-                console.log("success");
-                var data = JSON.parse(xhr_zones.responseText);
-                zones = data;
-            } else {
-                alert(xhr_zones.response);
-            }
+var xhr_zones = new XMLHttpRequest();
+xhr_zones.onreadystatechange = function () {
+    if(xhr_zones.readyState === 4) {
+        if (xhr_zones.status === 200){
+            console.log("01 - Ajax request for zoning_data.json");
+            var data = JSON.parse(xhr_zones.responseText);
+            zones = data;
+            console.log("Zipcode pulled from first record: " + zones.features[0].properties.ZIPCODE);
+        } else {
+            alert(xhr_zones.response);
         }
-    };
-    xhr_zones.open('GET', 'data/zoning_data.json');
-    xhr_zones.send();
-}
+    }
+};
+xhr_zones.open('GET', 'data/zoning_data.json');
+xhr_zones.send();
 
-function getHomes () {
-    var xhr_homes = new XMLHttpRequest();
-    xhr_homes.onreadystatechange = function () {
-        if(xhr_homes.readyState === 4) {
-            if(xhr_homes.status === 200) {
-                var data = JSON.parse(xhr_homes.responseText);
-                homes = data;
-            } else {
-                alert(xhr_homes.response);
-            }
+var xhr_homes = new XMLHttpRequest();
+xhr_homes.onreadystatechange = function () {
+    if(xhr_homes.readyState === 4) {
+        if(xhr_homes.status === 200) {
+            console.log("02 - Ajax request for homes.json");
+            var data = JSON.parse(xhr_homes.responseText);
+            homes = data;
+            console.log("Zipcode pulled from first record: " + homes[0].zip);
+        } else {
+            alert(xhr_homes.response);
         }
-    };
-    xhr_homes.open('GET', 'data/homes.json');
-    xhr_homes.send();
-}
+    }
+};
+xhr_homes.open('GET', 'data/homes.json');
+xhr_homes.send();
 
-main();
+console.log("03 - Attempt to log zoning data outside of Ajax");
+//console.log("Zipcode pulled from first record: " + zones.features[0].properties.ZIPCODE);
+
+console.log("04 - Attempt to log homes data outside of Ajax");
+console.log("Zipcode pulled from first record: " + homes[0].county);
